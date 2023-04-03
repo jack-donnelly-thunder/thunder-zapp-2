9bcdfa392bc9b29d9f1227e3e0e75ea54f1b50cc
jack-donnelly-thunder <90464939+jack-donnelly-thunder@users.noreply.github.com>
2023-04-03 13:12:18 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,264 @@
+const { App, AwsLambdaReceiver } = require('@slack/bolt');
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+  apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+/* 
+This sample slack application uses SocketMode
+For the companion getting started setup guide, 
+see: https://slack.dev/bolt-js/tutorial/getting-started 
+*/
+const awsLambdaReceiver = new AwsLambdaReceiver({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+// Initializes your app with your bot token and app token
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver: awsLambdaReceiver,
+});
+app.event('app_mention', async ({ event, content, say }) => {
+  try{
+    const appID = 'A051199E413';
+      // Start a thread on the original message
+    
+    var messages = [
+      
+    ]
+    if (!event.thread_ts) {
+      console.log("no thread hit" + event.text);
+      messages.push({"role": "user", "content": event.text.split('> ')[1]});
+      console.log(JSON.stringify(messages));
+      console.log(event.text.split('> ')[1]);
+      const result = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+      await app.client.chat.postMessage({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: event.channel,
+        thread_ts: event.ts,
+        text: result.data.choices[0].message.content
+      });
+    } 
+    else {
+      const result = await app.client.conversations.history({
+        channel: event.channel,
+        latest: event.ts,
+        limit: 1,
+      });
+      const threadstart = result.messages;
+      const originalMessage = threadstart[0];
+      console.log('second call' + JSON.stringify(threadstart));
+      const threadResult = await app.client.conversations.replies({
+        channel: event.channel,
+        ts: originalMessage.ts,
+      });
+      console.log('Thread length' + JSON.stringify(threadResult.messages));
+      const totalThread = threadResult.messages
+      for(const tmessage of totalThread ){
+        //HERE IS THE ISSUE WITH BOT CHECK
+        if (undefined != tmessage.bot_id){
+          messages.push({"role": "assistant", "content": tmessage.text});
+          console.log('Thread bot ' + tmessage.text);
+        }
+        else{
+          messages.push({"role": "user", "content": tmessage.text.split('> ')[1]});
+          console.log('Thread human ' + tmessage.text);
+
+        }
+      }
+      console.log(JSON.stringify(messages));
+      //console.log('threaded messages ' + JSON.stringify(messages));
+      const chatresult = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+      // If there are existing replies, add a new message to the thread
+      await app.client.chat.postMessage({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: event.channel,
+        thread_ts: event.ts,
+        text: chatresult.data.choices[0].message.content
+      });
+    }
+  //await say(result.data.choices[0].message.content);
+ 
+}
+  catch(error){
+    await say(error.message);
+  }
+});
+app.event('message', async ({ event, context }) => {
+  if (event.channel_type === 'im') {
+    console.log('DM Hit');
+    try{
+    
+      const appID = 'A051199E413';
+        // Start a thread on the original message
+      
+      var messages = [
+        
+      ]
+      if (!event.thread_ts) {
+        console.log("no thread hit");
+        messages.push({"role": "user", "content": event.text});
+  
+        const result = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: messages,
+        });
+        console.log(JSON.stringify(messages));
+        await app.client.chat.postMessage({
+          token: process.env.SLACK_BOT_TOKEN,
+          channel: event.channel,
+          thread_ts: event.ts,
+          text: result.data.choices[0].message.content
+        });
+      } 
+      else {
+        const result = await app.client.conversations.history({
+          channel: event.channel,
+          latest: event.ts,
+          limit: 1,
+        });
+        const threadstart = result.messages;
+        const originalMessage = threadstart[0];
+        console.log('second call' + JSON.stringify(threadstart));
+        const threadResult = await app.client.conversations.replies({
+          channel: event.channel,
+          ts: originalMessage.ts,
+        });
+        console.log('Thread length' + JSON.stringify(threadResult.messages));
+        const totalThread = threadResult.messages
+        for(const tmessage of totalThread ){
+          //HERE IS THE ISSUE WITH BOT CHECK
+          if (undefined != tmessage.bot_id){
+            messages.push({"role": "assistant", "content": tmessage.text});
+            console.log('Thread bot ' + tmessage.text);
+          }
+          else{
+            messages.push({"role": "user", "content": tmessage.text});
+            console.log('Thread human ' + tmessage.text);
+  
+          }
+        }
+        console.log(JSON.stringify(messages));
+        //console.log('threaded messages ' + JSON.stringify(messages));
+        const chatresult = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: messages,
+        });
+        // If there are existing replies, add a new message to the thread
+        await app.client.chat.postMessage({
+          token: process.env.SLACK_BOT_TOKEN,
+          channel: event.channel,
+          thread_ts: event.ts,
+          text: chatresult.data.choices[0].message.content
+        });
+      }
+    //await say(result.data.choices[0].message.content);
+   
+  }
+    catch(error){
+      await app.client.chat.postMessage({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: event.channel,
+        thread_ts: event.ts,
+        text: error.message
+      });
+    }
+  }
+});
+// Listens to incoming messages that contain "hello"
+/*app.message('Zapp: ', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+
+  try{
+    
+    const appID = 'A051199E413';
+      // Start a thread on the original message
+    
+    var messages = [
+      
+    ]
+    if (!message.thread_ts) {
+      console.log("no thread hit");
+      messages.push({"role": "user", "content": message.text.split('Zapp: ')[1]});
+
+      const result = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+      console.log(JSON.stringify(messages));
+      await app.client.chat.postMessage({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: message.channel,
+        thread_ts: message.ts,
+        text: result.data.choices[0].message.content
+      });
+    } 
+    else {
+      const result = await app.client.conversations.history({
+        channel: message.channel,
+        latest: message.ts,
+        limit: 1,
+      });
+      const threadstart = result.messages;
+      const originalMessage = threadstart[0];
+      console.log('second call' + JSON.stringify(threadstart));
+      const threadResult = await app.client.conversations.replies({
+        channel: message.channel,
+        ts: originalMessage.ts,
+      });
+      console.log('Thread length' + JSON.stringify(threadResult.messages));
+      const totalThread = threadResult.messages
+      for(const tmessage of totalThread ){
+        //HERE IS THE ISSUE WITH BOT CHECK
+        if (undefined != tmessage.bot_id){
+          messages.push({"role": "assistant", "content": tmessage.text});
+          console.log('Thread bot ' + tmessage.text);
+        }
+        else{
+          messages.push({"role": "user", "content": tmessage.text.split('Zapp: ')[1]});
+          console.log('Thread human ' + tmessage.text);
+
+        }
+      }
+      console.log(JSON.stringify(messages));
+      //console.log('threaded messages ' + JSON.stringify(messages));
+      const chatresult = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+      // If there are existing replies, add a new message to the thread
+      await app.client.chat.postMessage({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: message.channel,
+        thread_ts: message.ts,
+        text: chatresult.data.choices[0].message.content
+      });
+    }
+  //await say(result.data.choices[0].message.content);
+ 
+}
+  catch(error){
+    await say(error.message);
+  }
+});*/
+
+
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
+
+module.exports.handler = async (event, context, callback) => {
+    const handler = await awsLambdaReceiver.start();
+    return handler(event, context, callback);
+}
